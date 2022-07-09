7655a547ee6766878c7fd49e0e59a87b4789c957
Amirhoseinjafare <aj24091387@gmail.com>
2022-07-09 17:49:09 +0430

    فایل های جدید

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+         <!-- <meta http-equiv="refresh" content="3"> -->
+</head>
+<body>
+
+<form action="login.php" method="GET">
+    <label for="usename">Username:</label>
+ <input type="text" name="usename"><br>
+ <label for="password">password</label>
+ <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Your usename is: <?php echo $_GET["usename"]; ?><br>
+Your password is: <?php echo $_GET["password"]; ?>
+</body>
+</html>
+
